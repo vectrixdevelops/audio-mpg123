ec61d5d88a5b5b79bd0740a8268acf12b246651f
Connor Hartley <vectrixu@gmail.com>
2016-11-08 09:57:10 +1300

    Use node-pre-gyp to require binding and add test

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-module.exports = require('bindings')('audio_mpg123')
+var path = require('path')
+
+module.exports = require(require('node-pre-gyp').find(path.resolve(path.join(__dirname, './package.json'))))
