536a0bd11dfe3420a60a88476a175dae1e9066f9
Jamen Marzonie <jamenmarz@gmail.com>
2017-03-14 21:30:11 -0500

    Remove print functions

--- src/binding.cpp
+++ src/binding.cpp
@@ -28,11 +28,11 @@ namespace {
         out123_handle *ao = out123_new();
 
         if(!ao) {
-            fprintf(stderr, "Failed to initialize output handle.\n");
+            // fprintf(stderr, "Failed to initialize output handle.\n");
             out123_del(ao);
             value = 0;
         } else if(out123_open(ao, NULL, NULL) != OUT123_OK) {
-            fprintf(stderr, "Failed to open output: %s\n", out123_strerror(ao));
+            // fprintf(stderr, "Failed to open output: %s\n", out123_strerror(ao));
             out123_del(ao);
             value = 0;
         } else {
@@ -58,10 +58,10 @@ namespace {
         const char *encname;
         encname = out123_enc_name(encoding);
 
-        printf("Playing with %i channel(s), %li Hz(s) and encoding %s.\n", channels, rate, encname ? encname : "???");
+        // printf("Playing with %i channel(s), %li Hz(s) and encoding %s.\n", channels, rate, encname ? encname : "???");
 
         if(out123_start(ao, rate, channels, encoding) || !ao) {
-            fprintf(stderr, "Failed to start output: %s\n", out123_strerror(ao));
+            // fprintf(stderr, "Failed to start output: %s\n", out123_strerror(ao));
             out123_del(ao);
             value = 0;
         } else {
@@ -168,10 +168,10 @@ namespace {
         out123_handle *ao = out123_new();
 
         if(!ao) {
-            fprintf(stderr, "Failed to initialize output handle.\n");
+            // fprintf(stderr, "Failed to initialize output handle.\n");
             out123_del(ao);
         } else if(out123_open(ao, NULL, NULL) != OUT123_OK) {
-            fprintf(stderr, "Failed to open output: %s\n", out123_strerror(ao));
+            // fprintf(stderr, "Failed to open output: %s\n", out123_strerror(ao));
             out123_del(ao);
         }
 
